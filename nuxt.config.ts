207e2919c61caca113f020b7fcc79e0f7bef9ba8
BetterThanDefault <betterthandefault@gmail.com>
2025-12-12 13:24:46 +0100

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,6 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/ui', '@nuxt/content']
+  modules: ['@nuxt/ui', '@nuxt/content'],
+  app: { baseURL: '/btd-docs/' }
 })
